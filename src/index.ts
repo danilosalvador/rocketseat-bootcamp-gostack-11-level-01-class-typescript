8b0263695be6f45a4645994a04d1e2a910278b05
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-06-02 23:24:41 -0300

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello TypeScript!'});
+});
+
+app.listen(3333, () => {
+  console.log('Go! Go! Go! 🚀');
+});
